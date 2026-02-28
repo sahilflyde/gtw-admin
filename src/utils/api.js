16d7b162c4ab1a619b40108ffde333f84c7f35b0
Sahil Kalkal <sahil@flyde.in>
2026-02-28 18:00:29 +0530

    changed api instance base url

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth.js";
 
 // const API_BASE_URL = 'http://localhost:5000/api';
-const API_BASE_URL = "https://gtw-backend-qhwv.onrender.com/api";
+const API_BASE_URL = "https://gtw-backend-m9qp.onrender.com/api";
 
 //api
 
